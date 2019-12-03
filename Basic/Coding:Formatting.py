f49b9ef23d9040ee99940bb04266c3fe1750c078
Fanfan <fanfan@hanhanebookpuro.speedport.ip>
2019-12-03 16:13:53 +0100

    Basic 2

---
+++ Basic/Coding:Formatting.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File -> save with encoding -> UTF-8 without BOM
+print('这是中文')
+#formatting, placeholder: %d->int, %f->float, %s->string,%x->hex, %%->%
+print('Hi, %s, you have $%d.'%('David',10000))
+
+#übung
+s1 = 71
+s2 = 85
+r = (s2 - s1)*100/s1
+print(r)
+print('Hi, your scores have been imporved in: %s %%' %r)
