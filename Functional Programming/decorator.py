3d84d06daf4e3d9ea7a2f20066dc599183573b6e
IngridFanfan <33351913+IngridFanfan@users.noreply.github.com>
2019-12-11 15:31:22 +0100

    Add files via upload

---
+++ Functional Programming/decorator.py
@@ -0,0 +1,65 @@
+#Decorator
+
+#由于函数也是一个对象，而且函数对象可以被赋值给变量，所以，通过变量也能调用该函数。
+
+def now():
+	print('2015-3-25')
+
+#f = now
+#f()
+#2015-3-25
+
+#函数对象有一个__name__属性，可以拿到函数的名字：
+#now.__name__
+#'now'
+#f.__name__
+#'now'
+
+#假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+#本质上，decorator就是一个返回函数的高阶函数。所以，我们要定义一个能打印日志的decorator，可以定义如下：
+
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+#调用now()函数，不仅会运行now()函数本身，还会在运行now()函数前打印一行日志：
+
+#now()
+#call now():
+#2015-3-25
+
+#由于log()是一个decorator，返回一个函数，所以，原来的now()函数仍然存在，只是现在同名的now变量指向了新的函数，于是调用now()将执行新函数，即在log()函数中返回的wrapper()函数。
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+#不需要编写wrapper.__name__ = func.__name__这样的代码，Python内置的functools.wraps就是干这个事的，所以，一个完整的decorator的写法如下：
+
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+#或者针对带参数的decorator：
+
+import functools
+
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
