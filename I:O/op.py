22ecab1cf83e169ab76e926e4a1b4f17c17841d2
IngridFanfan <33351913+IngridFanfan@users.noreply.github.com>
2019-12-12 15:07:10 +0100

    Add files via upload

---
+++ I:O/op.py
@@ -0,0 +1,23 @@
+import os
+
+# 查看当前目录的绝对路径:
+os.path.abspath('.')
+'/Users/michael'
+# 在某个目录下创建一个新目录，首先把新目录的完整路径表示出来:
+os.path.join('/Users/michael', 'testdir')
+'/Users/michael/testdir'
+# 然后创建一个目录:
+os.mkdir('/Users/michael/testdir')
+# 删掉一个目录:
+os.rmdir('/Users/michael/testdir')
+
+# 查看当前目录的绝对路径:
+os.path.abspath('.')
+'/Users/michael'
+# 在某个目录下创建一个新目录，首先把新目录的完整路径表示出来:
+os.path.join('/Users/michael', 'testdir')
+'/Users/michael/testdir'
+# 然后创建一个目录:
+os.mkdir('/Users/michael/testdir')
+# 删掉一个目录:
+os.rmdir('/Users/michael/testdir')
